c2b83177906f16ac1c05986f56f71a864d9d2aae
Paal A <pgu070@live.no>
2016-03-18 13:01:48 +0000

    jazzycomputerwillunderstand000         .

--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -9,23 +9,36 @@ var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
+    
     this.setState({open : !this.state.open});
 
     
   },
   getInitialState: function(){
-    return { open: true }
+    return { open: false }
+  },
+  handleItemClick: function(item) {
+    this.setState({
+      
+      open:false,
+      itemTitle: item
+    });
+    
   },
   render: function() {
     var list = this.props.items.map(function(item){
-      return <ListItem item={item} />
-    });
+      return <ListItem 
+      item={item} 
+      whenItemClicked={this.handleItemClick} 
+      className={this.state.itemTitle === item ? "active" : ""}
+      />
+    }.bind(this));
 
     return <div className="dropdown">
       <Button
         whenClicked={this.handleClick}
         className="btn-default"
-        title={this.props.title}
+        title={this.state.itemTitle || this.props.title}
         subTitleClassName="caret"
         />
         <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
